95b0449df21c6ee6c0373748c22e79b753b312d8
skyrior <12475669+skyrior@users.noreply.github.com>
2018-04-06 10:26:23 -0300

    Async Writes in Redux

---
+++ Templates/React Class Component.js
@@ -0,0 +1,13 @@
+import React, {PropTypes} from 'react';
+
+class ManageCoursePage extends React.Component {
+  render () {
+
+  }
+}
+
+ManageCoursePage.propTypes = {
+  // myProp: PropTypes.string.isRequired
+};
+
+export default ManageCoursePage;
---
+++ Templates/React Stateless Component.js
@@ -0,0 +1,13 @@
+import React, {PropTypes} from 'react';
+
+const ManageCoursePage = (props) => {
+  return (
+
+  );
+}
+
+ManageCoursePage.propTypes = {
+  // myProp: PropTypes.string.isRequired
+};
+
+export default ManageCoursePage;
